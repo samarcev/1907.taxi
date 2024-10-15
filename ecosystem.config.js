0b05c5662e62b413f610f62e7dc3ef1811058296
Максим Самарцев <web.samarcev@gmail.com>
2024-10-15 08:58:24 +0500

    ci/cd #38

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,21 @@
 module.exports = {
     apps: [
         {
-            name: '1907.taxi :3000',
+            name: '1907.taxi :3000', // Имя приложения
             port: '3000',
-            exec_mode: 'cluster',
-            instances: '1',
-            script: './.output/server/index.mjs'
-        }
+            script: '.output/server/index.mjs', // Путь к вашему собранному приложению
+            instances: '1', // Автоматическое определение количества инстансов
+            exec_mode: 'cluster', // Режим запуска в кластере
+            watch: false, // Отключить слежение за изменениями в файлах (обычно не нужно на продакшене)
+            log_file: './logs/combined.log', // Файл для общего логирования
+            out_file: './logs/out.log', // Файл для логирования стандартного вывода
+            error_file: './logs/error.log', // Файл для логирования ошибок
+            env: {
+                NODE_ENV: 'production', // Переменная окружения для продакшена
+            },
+            env_development: {
+                NODE_ENV: 'development', // Переменная для разработки
+            },
+        },
     ]
 }
